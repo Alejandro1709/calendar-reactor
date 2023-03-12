3163589f752c4f2c933da39510745823b545d996
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-12 16:30:49 -0500

    Add: Ability to open/close Modal

--- src/components/auth/Logged.tsx
+++ src/components/auth/Logged.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import useModal from '@/hooks/useModal';
 import { signOut } from 'next-auth/react';
 import Image from 'next/image';
 
 export default function Logged({ image }: { image: string }) {
+  const { openModal } = useModal();
+
   return (
     <ul className='flex flex-row gap-4'>
-      <button className='bg-gray-600 text-white rounded-md p-2 hover:bg-gray-500'>
+      <button
+        className='bg-gray-600 text-white rounded-md p-2 hover:bg-gray-500'
+        onClick={openModal}
+      >
         Submit
       </button>
       <button
